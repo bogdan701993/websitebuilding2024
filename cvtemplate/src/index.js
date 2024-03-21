70e5419ddd3527b1ec8881dc623279d8746fdfc7
bogdan701993 <150378597+bogdan701993@users.noreply.github.com>
2024-03-21 16:09:12 +0200

    websitebuilding2024

--- cvtemplate/src/index.js
+++ cvtemplate/src/index.js
@@ -4,10 +4,17 @@ import './scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
+// Import https://github.com/VincentGarreau/particles.js
+
 import particles from 'particles.js'
 import './js/particles.js'
 
 adaugam in index.js 
+
+// Import https://github.com/mattboldt/typed.js/
+// import Typed from 'typed.js';
  import '../node_modules/typed.js/dist/typed.umd.js'
 
  import './js/main.js'
+ import './js/skill.js'
+
--- cvtemplate/src/js/particles.js
+++ cvtemplate/src/js/particles.js
@@ -10,7 +10,7 @@ particlesJS("particles-js", {
             }
         },
         "color": {
-            "value": "#04b962"
+            "value": "#479fc8"
         },
         "shape": {
             "type": "circle",
